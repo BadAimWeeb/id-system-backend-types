97a4964aa1febe4bb7599372435c13076e1975ac
BAW_CI_AUTOMATION <noreply+bot@badaimweeb.me>
2024-02-20 11:21:12 +0000

    Publish automated build result from 1.0.3+9e20ea5 main

--- dist/ws-api/main/main_generatePasskeyChallenge.d.ts
+++ dist/ws-api/main/main_generatePasskeyChallenge.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="ws" />
 /// <reference types="node" />
-declare const procedure: import("@badaimweeb/js-dtsocket").Procedure<"verify" | "enroll" | "login" | "enroll-verify", import("@simplewebauthn/typescript-types").PublicKeyCredentialCreationOptionsJSON | import("@simplewebauthn/typescript-types").PublicKeyCredentialRequestOptionsJSON | (import("@simplewebauthn/typescript-types").PublicKeyCredentialCreationOptionsJSON | import("@simplewebauthn/typescript-types").PublicKeyCredentialRequestOptionsJSON)[], import("@badaimweeb/js-dtsocket").ServerContext<import("../../types.js").GlobalState, import("../../types.js").LocalState, import("../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
+declare const procedure: import("@badaimweeb/js-dtsocket").Procedure<"verify" | "enroll" | "login" | "enroll-verify", import("@simplewebauthn/types").PublicKeyCredentialCreationOptionsJSON | import("@simplewebauthn/types").PublicKeyCredentialRequestOptionsJSON | (import("@simplewebauthn/types").PublicKeyCredentialCreationOptionsJSON | import("@simplewebauthn/types").PublicKeyCredentialRequestOptionsJSON)[], import("@badaimweeb/js-dtsocket").ServerContext<import("../../types.js").GlobalState, import("../../types.js").LocalState, import("../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
     req: import("http").IncomingMessage;
 }>>>;
 export default procedure;
