a0dfd035be6f6828441561df67b05fd4bc8cdd0e
BAW_CI_AUTOMATION <noreply+bot@badaimweeb.me>
2024-06-27 00:57:59 +0000

    Publish automated build result from 1.2.1+8a50f17 main

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,9 +3,33 @@ import "./scripts/databaseMigration.js";
 import "./scripts/cleaningClock.js";
 import "./scripts/autoupdate.js";
 import { type Session } from "@badaimweeb/js-protov2d";
-import { DTSocketServer, type ServerContext } from "@badaimweeb/js-dtsocket";
+import { type ServerContext } from "@badaimweeb/js-dtsocket";
 import * as WSAPI from "./ws-api/index.js";
 import type { GlobalState, LocalState, EventTable } from "./types.js";
-declare const apiServer: DTSocketServer<ServerContext<GlobalState, LocalState, EventTable, Session<any>, typeof WSAPI>>;
+declare const apiServer: import("@badaimweeb/js-dtsocket/dist/server.js").DTSocketServerInterface<ServerContext<GlobalState, LocalState, EventTable, Session<any>, typeof WSAPI>, {
+    cs: {} & {
+        "internal:new-session": [import("@badaimweeb/js-dtsocket/dist/server_csocket.js").DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<any>, typeof WSAPI>, {
+            cs: {
+                [x: string]: unknown[];
+            } & {
+                "internal:drop": [];
+            };
+            sc: {
+                [x: string]: unknown[];
+            };
+        }>];
+        "internal:remove-session": [import("@badaimweeb/js-dtsocket/dist/server_csocket.js").DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<any>, typeof WSAPI>, {
+            cs: {
+                [x: string]: unknown[];
+            } & {
+                "internal:drop": [];
+            };
+            sc: {
+                [x: string]: unknown[];
+            };
+        }>];
+    };
+    sc: {};
+}>;
 export type Server = typeof apiServer;
 export {};
